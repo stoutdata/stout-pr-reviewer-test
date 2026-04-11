e96237a9abd7f5c0d407c8423ee2d34033141a4f
DanielRMErskine <danielrmerskine63@gmail.com>
2026-04-10 19:02:50 -0700

    Wire UART debug output into boot sequence

--- main.c
+++ main.c
@@ -12,3 +12,11 @@ void setup(void) {
     gpio_init();
     gpio_set_led(1);
 }
+
+extern void uart_init(uint32_t baudrate);
+extern void uart_send_string(const char *str);
+
+void debug_init(void) {
+    uart_init(115200);
+    uart_send_string("Boot OK\r\n");
+}
